00cada376e8e1af7d9f64007e1fbd8351533142b
Michael Auclair <mike@michael-auclairs-macbook.local>
2010-04-17 09:14:12 -0400

    Framing out the example markup to build the js in

---
+++ jquery.cdc.ticker.js
@@ -0,0 +1,11 @@
+/* 
+ * jQuery CDC Ticker
+ * by Mike Auclair
+ * mike@mikeauclair.com
+ *
+ * Copyright (c) 2010 Mike Auclair
+ * Licensed under the GPL (gpl-2.0.txt) license. 
+ *
+ * NOTE: This plugin depends on jQuery.  Download jQuery at www.jquery.com
+ *
+ */
