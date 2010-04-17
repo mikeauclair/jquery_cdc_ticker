363e4fed5531495875643770d164de66d6045bd4
Michael Auclair <mike@michael-auclairs-macbook.local>
2010-04-17 10:03:28 -0400

    Example images, skeleton jquery plugin

--- jquery.cdc.ticker.js
+++ jquery.cdc.ticker.js
@@ -9,3 +9,38 @@
  * NOTE: This plugin depends on jQuery.  Download jQuery at www.jquery.com
  *
  */
+
+
+/*
+SCRATCH PAD
+
+position:absolute;
+left:0;
+top:0;
+z-index:5;
+*/
+(function(jQuery)
+{
+	var self = null;
+	jQuery.fn.cdcticker = function(o)
+	{
+		return this.each(function()
+		{
+			new jQuery.cdcticker(this, o);
+		});
+	};
+	jQuery.cdcticker = function (e, o)
+	{
+		this.init();
+	};
+	jQuery.cdcticker.fn = jQuery.cdcticker.prototype = {
+		cdcticker: '0.0.1'
+	};
+	jQuery.cdcticker.fn.extend = jQuery.cdcticker.extend = jQuery.extend;
+	jQuery.cdcticker.fn.extend({
+		init: function()
+		{
+			var self = this;
+		}
+	});
+})(jQuery);
